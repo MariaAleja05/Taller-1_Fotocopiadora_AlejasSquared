20aa4e33d90d00beb831ff1a100a93d6b8bc0a39
mvarelau <141885396+mvarelau@users.noreply.github.com>
2023-09-14 17:46:03 -0500

    Punto 6

---
+++ Sonidos_y_maneras.py
@@ -0,0 +1,10 @@
+print("-------------------------------------------------------")
+print("Determinar si una letra es vocal o consonante")
+print("-------------------------------------------------------")
+#Entradas
+n=( input("Escriba una letra: "))
+#Proceso
+if n == "a" or n == "e" or n == "i" or n == "o" or n == "u" or n == "E" or n == "I" or n == "O" or n == "U" or n == "A":
+    print(f"{n} Es una vocal")
+elif n == "q" or n == "w"  or n == "r" or n == "t" or n == "y" or n == "p" or n == "s" or n == "d" or n == "f" or n == "g"or n == "h"or n == "j"or n == "k"or n == "l"or n == "z"or n == "x"or n == "c"or n == "v"or n == "b"or n == "n"or n == "m" or n == "Q" or n == "W"  or n == "R" or n == "T" or n == "Y" or n == "P" or n == "S" or n == "D" or n == "F" or n == "G"or n == "H"or n == "J"or n == "K"or n == "L"or n == "Z"or n == "X"or n == "C"or n == "V"or n == "B"or n == "N"or n == "M":
+    print(f"{n} Es una consonante")
